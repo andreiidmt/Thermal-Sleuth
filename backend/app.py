2fa04a04c4c3a277b2d4523f26f0f8b9549808c5
andreiidmt <andreiidmt@gmail.com>
2026-04-25 14:23:54 +0300

    added cors safety

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import engine, SessionLocal, Base
 import models, schemas, crud
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # This allows any website to talk to your backend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency to get DB session
 def get_db():
